4286bed5a8b4fcfa359513b230715498e07cac72
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-11-29 12:54:56 +0530

    Update 58.py

--- 58.py
+++ 58.py
@@ -1,6 +1,6 @@
-p,q=map(int,raw_input().split()) 
-l=[int(j) for j in raw_input().split()] 
-if p and q in l: 
+m,n=map(int,raw_input().split()) 
+r=[int(i) for i in raw_input().split()] 
+if m and n in l: 
      print "yes" 
 else: 
      print "no"
