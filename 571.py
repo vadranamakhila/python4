74603ba6f7a50438a92347aa95ebefe22f210d3b
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-11-29 12:50:50 +0530

    Update 571.py

--- 571.py
+++ 571.py
@@ -1,10 +1,10 @@
-n,k=map(int,raw_input().split())
-list=[int(x) for x in raw_input().split()]
-if n in list:
-    count=list.count(n)
+m,n=map(int,raw_input().split())
+list=[int(i) for i in raw_input().split()]
+if m in list:
+    count=list.count(m)
     print count
-elif k in list:
-    count=list.count(k)
+elif n in list:
+    count=list.count(n)
     print count
 else:
     print (0)
